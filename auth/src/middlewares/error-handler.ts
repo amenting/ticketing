07006edb9c5715e5e4566532d2fd20553edeb2f3
Arne Menting <a.menting@itbalance.de>
2021-03-03 16:28:15 +0100

    error handling middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import {Request, Response, NextFunction} from 'express';
+import {RequestValidationError} from '../errors/request-validation-error';
+import {DatabaseConnectionError} from '../errors/database-connection-error';
 
 export const errorHandler = (
     err: Error, 
@@ -6,9 +8,13 @@ export const errorHandler = (
     res: Response, 
     next: NextFunction
 ) => {
-    console.log('Something went wrong', err);
-
+    if (err instanceof RequestValidationError) {
+        console.log('handling req valid error');
+    }
+    if (err instanceof DatabaseConnectionError) {
+        console.log('handling db con error');
+    }
     res.status(400).send({
-        message: 'Something went wrong'
+        message: err.message
     });
 }
