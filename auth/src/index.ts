d2265c494e4438bb50a7e50866028f5270cd19f5
Arne Menting <a.menting@itbalance.de>
2021-03-04 00:22:29 +0100

    authentication setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -31,6 +31,11 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined. '+
+            'Try: kubectl create secret generic jwt-secret --from-literal=JWT_KEY=somekey');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
             useNewUrlParser: true,
