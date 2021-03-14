9692a71a2cada2da211f0df6e7070e494c3cef25
Arne Menting <a.menting@itbalance.de>
2021-03-14 12:30:20 +0100

    ponder external name service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,18 @@
-export default () => {
+import axios from 'axios';
+
+// could create an external name service to map that name to a simpler domain
+const BASE = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>;
 }
+
+LandingPage.getInitialProps = async ({request}) => {
+    const cookie = 'todo';
+    const response = await axios.get(BASE + '/api/users/currentuser');
+    console.log(response);
+    return response.data;
+};
+
+export default LandingPage;
