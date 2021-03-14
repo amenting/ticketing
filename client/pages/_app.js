c0a5a94ad4a369d08a2b3224b5a8046bd60baaf7
Arne Menting <a.menting@itbalance.de>
2021-03-14 13:39:47 +0100

    custom app init props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const AppConfig =  ({ Component, pageProps}) => {
-    return <Component {...pageProps} />
+const AppComponent =  ({ Component, pageProps, currentUser}) => {
+    return <div>
+        <h1>Header {currentUser?.email}</h1>
+        <Component {...pageProps} />
+    </div>;
 }
 
-export default AppConfig;
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+    const { data } = await client.get('/api/users/currentUser');
+    let pageProps = {};
+    if(appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+    return {
+        pageProps,
+        currentUser: data.currentUser // {currentUser: {...}}
+    }; 
+};
+
+export default AppComponent;
