defe1e519b1775eabe7ec68df2bdc0b2149f185a
Arne Menting <a.menting@itbalance.de>
2021-03-17 22:32:44 +0100

    close down stan correctly

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -11,7 +11,22 @@ const stan = nats.connect('ticketing', clientId, {
 stan.on('connect', () => {
     console.log(`Listener connected to NATS - clientId: ${clientId}`);
 
-    const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        process.exit();
+    });
+
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true);
+    // monitoring:
+    // kubectl get pods
+    // kubectl port-forward my-special-pod 8222:8222
+    //  http://localhost:8222/streaming/channelsz?subs=1
+    const subscription = stan.subscribe(
+        'ticket:created',
+        'orders-service-queue-group',
+        options
+    );
 
     subscription.on('message', (msg: Message) => {
         const data = msg.getData();
@@ -19,5 +34,10 @@ stan.on('connect', () => {
         if (typeof data === 'string') {
             console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
         }
+
+        msg.ack();
     });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
