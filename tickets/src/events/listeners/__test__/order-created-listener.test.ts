879c61ee8a4e42fb71dddf47341009dadc8911f7
Arne Menting <a.menting@itbalance.de>
2021-03-26 12:10:35 +0100

    test mocked function arguments

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -56,5 +56,15 @@ it('acks the message', async () => {
     expect(msg.ack).toHaveBeenCalled();
 });
 
+it('publishes a ticket updated event', async() => {
+    const { listener, ticket, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+    const json = JSON.parse((natsWrapper.client.publish as jest.Mock).mock.calls[0][1]);
+    expect(json.orderId).toEqual(data.id);
+});
+
 
 
